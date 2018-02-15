2963a3c970a9e4c9be5c2b4b1b00b6884c40a96f
Patrick Schmidt <36363497+wegberger94@users.noreply.github.com>
2018-02-15 19:01:47 +0900

    qwe
    asd

---
+++ foo/src/why.rs
@@ -0,0 +1,28 @@
+fn erste_rechnung(x:u32)  -> u32{
+       let mut y = x;
+       if (x > 4)
+       {
+           y = y - 4;
+       }
+   return y;
+}
+
+fn main() {
+   let mut zahl = String::new();
+   let x;
+
+   println!("Geben Sie eine Zahl ein: ");
+
+   std::io::stdin().read_line(&mut zahl)
+      .expect("Failed to read line");
+
+   println!("{}",  zahl);
+
+   let zahl2: u32 = zahl.trim().parse()
+       .expect("Please type a number!");
+
+   x = erste_rechnung(zahl2);
+
+
+   println!("{}",  x);
+}
